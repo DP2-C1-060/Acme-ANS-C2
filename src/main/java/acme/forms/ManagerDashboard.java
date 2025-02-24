ea3b491e72b234de580e45e2a098891f672ba86f
javmunrom <javmunrom@alum.us.es>
2025-02-24 18:47:58 +0100

    Task 15: Implement manager dashboards

--- src/main/java/acme/forms/ManagerDashboard.java
+++ src/main/java/acme/forms/ManagerDashboard.java
@@ -14,31 +14,22 @@ public class ManagerDashboard extends AbstractForm {
 
 	// Attributes -------------------------------------------------------------
 
-	// Indicadores del manager
 	private Integer				rankingPosition;
 	private Integer				yearsToRetirement;
 	private Double				ratioOnTimeLegs;
 	private Double				ratioDelayedLegs;
 
-	// Indicadores de popularidad de aeropuertos
 	private String				mostPopularAirport;
 	private String				leastPopularAirport;
 
-	// Número de legs según su estado (correspondientes a ON_TIME, DELAYED, CANCELLED, LANDED)
 	private Integer				legsOnTime;
 	private Integer				legsDelayed;
 	private Integer				legsCancelled;
 	private Integer				legsLanded;
 
-	// Estadísticas del coste de los vuelos
 	private Double				averageFlightCost;
 	private Double				minFlightCost;
 	private Double				maxFlightCost;
 	private Double				stdDevFlightCost;
 
-	// Derived attributes -----------------------------------------------------
-	// (No se definen métodos derivados en este formulario)
-
-	// Relationships ----------------------------------------------------------
-	// (Si fuese necesario, se definirían relaciones con otras entidades o formularios)
 }
