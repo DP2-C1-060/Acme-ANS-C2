de24e1297a86002c1427f276f68f3b1a8ec2067b
javmunrom <javmunrom@alum.us.es>
2025-02-27 19:03:21 +0100

    Task 05: Track individual flight legs

--- src/main/java/acme/entities/legs/Leg.java
+++ src/main/java/acme/entities/legs/Leg.java
@@ -14,7 +14,7 @@ import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidMoment;
-import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidNumber;
 import acme.constraints.ValidShortText;
 import acme.entities.flights.Flight;
 import lombok.Getter;
@@ -50,7 +50,7 @@ public class Leg extends AbstractEntity {
 	private Date				scheduledArrival;
 
 	@Mandatory
-	@ValidScore
+	@ValidNumber(min = 0.1, max = 24, integer = 2, fraction = 2)
 	@Automapped
 	private double				duration; // en horas
 
