9de5b2ff5657a0fa2b51a98a6c51505cf2fb1597
Sergio Manuel Conde Bajo <serconbaj@alum.us.es>
2025-03-11 09:32:31 +0100

    Task 4: Define and store claim data
    Refs #140

---
+++ src/main/java/acme/entities/claim/Claim.java
@@ -0,0 +1,66 @@
+
+package acme.entities.claim;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.constraints.ValidLongText;
+import acme.realms.Agent;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Claim extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	@Automapped
+	private Date				registrationMoment;
+
+	@Mandatory
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Mandatory
+	@ValidLongText
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private ClaimType			type;
+
+	@Optional
+	@Valid
+	@Automapped
+	private Boolean				accepted;
+
+	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@ManyToOne(optional = false)
+	@Valid
+	private Agent				agent;
+
+}
