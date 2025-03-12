7b9d4e404989b3bea1b3ac26b4e3b39451aec4bf
javmunrom <javmunrom@alum.us.es>
2025-03-12 14:52:00 +0100

    Leg fix

--- src/main/java/acme/entities/legs/Leg.java
+++ src/main/java/acme/entities/legs/Leg.java
@@ -1,25 +1,25 @@
 
 package acme.entities.legs;
 
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidMoment;
-import acme.client.components.validation.ValidNumber;
-import acme.client.components.validation.ValidString;
-import acme.constraints.ValidShortText;
-import acme.constraints.leg.ValidDistinctAirports;
-import acme.constraints.leg.ValidFlightNumberPrefix;
-import acme.constraints.leg.ValidScheduledDates;
-import acme.constraints.leg.ValidUniqueFlightNumberDigits;
+import acme.constraints.leg.ValidFlightNumber;
+import acme.constraints.leg.ValidLeg;
 import acme.entities.aircraft.Aircraft;
 import acme.entities.airport.Airport;
 import acme.entities.flights.Flight;
@@ -29,10 +29,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@ValidUniqueFlightNumberDigits
-@ValidScheduledDates
-@ValidDistinctAirports
-@ValidFlightNumberPrefix
+@ValidLeg
 public class Leg extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
@@ -41,55 +38,63 @@ public class Leg extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@Mandatory
-	@ValidShortText
-	@Automapped
-	@ValidString(pattern = "^[A-Z]{3}\\d{4}$")
+	@ValidFlightNumber
+	@Column(unique = true)
 	private String				flightNumber;
 
 	@Mandatory
-	@ValidMoment
+	@ValidMoment(past = false)
 	@Temporal(TemporalType.TIMESTAMP)
-	@Automapped
 	private Date				scheduledDeparture;
 
 	@Mandatory
-	@ValidMoment
+	@ValidMoment(past = false)
 	@Temporal(TemporalType.TIMESTAMP)
-	@Automapped
 	private Date				scheduledArrival;
 
 	@Mandatory
 	@Automapped
-	@ValidNumber(min = 1, max = 1000, integer = 4)
-	private Integer				duration; // en minutos
-
-	@Mandatory
-	@Automapped
+	@Valid
 	private LegStatus			status;
 
 	@Mandatory
 	@Automapped
 	private boolean				draftMode;
 
+	// Derived attributes -----------------------------------------------------
+
+
+	@Transient
+	public Double getDuration() {
+
+		ZonedDateTime departure = this.scheduledDeparture.toInstant().atZone(ZoneId.systemDefault());
+		ZonedDateTime arrival = this.scheduledArrival.toInstant().atZone(ZoneId.systemDefault());
+
+		Duration duration = Duration.between(departure, arrival);
+
+		return (double) duration.toMinutes();
+	}
+
 	// Relationships ----------------------------------------------------------
 
-	@Mandatory
-	@ManyToOne(optional = false)
-	@Valid
-	private Airport				departureAirport;
 
 	@Mandatory
 	@ManyToOne(optional = false)
 	@Valid
-	private Airport				arrivalAirport;
+	private Airport		departureAirport;
 
 	@Mandatory
 	@ManyToOne(optional = false)
 	@Valid
-	private Aircraft			aircraft;
+	private Airport		arrivalAirport;
 
 	@Mandatory
 	@ManyToOne(optional = false)
 	@Valid
-	private Flight				flight;
+	private Aircraft	aircraft;
+
+	@Mandatory
+	@ManyToOne(optional = false)
+	@Valid
+	private Flight		flight;
 }
