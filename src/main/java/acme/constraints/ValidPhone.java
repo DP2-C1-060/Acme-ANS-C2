752e12eaf82baa11b7f0210ec8059e1aecae9e96
Ivan Frutos <ivafrurai@alum.us.es>
2025-03-12 17:40:06 +0100

    130-task-03-Implement-Technician-Entity-with-validation-constraints

---
+++ src/main/java/acme/constraints/ValidPhone.java
@@ -0,0 +1,39 @@
+
+package acme.constraints;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+
+@Target({
+	ElementType.FIELD, ElementType.PARAMETER
+})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ValidPhone.PhoneNumberValidatorImpl.class)
+public @interface ValidPhone {
+
+	String message() default "El número de teléfono debe seguir el patrón: + seguido de 6 a 15 dígitos.";
+	Class<?>[] groups() default {};
+	Class<? extends Payload>[] payload() default {};
+
+
+	class PhoneNumberValidatorImpl implements ConstraintValidator<ValidPhone, String> {
+
+		@Override
+		public void initialize(final ValidPhone constraintAnnotation) {
+		}
+
+		@Override
+		public boolean isValid(final String value, final ConstraintValidatorContext context) {
+			if (value == null)
+				return true;
+			return value.matches("^\\+?\\d{6,15}$");
+		}
+	}
+}
---
+++ src/main/java/acme/realms/Technician.java
@@ -0,0 +1,55 @@
+
+package acme.realms;
+
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidString;
+import acme.constraints.ValidLongText;
+import acme.constraints.ValidPhone;
+import acme.constraints.ValidShortText;
+import acme.constraints.ValidYearsOfExperience;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Technician extends AbstractRole {
+
+	// Serialisation version --------------------------------------------------
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$", message = "El número de licencia debe seguir el patrón: 2-3 letras seguidas de 6 dígitos.")
+	@Automapped
+	private String				licenseNumber;
+
+	@Mandatory
+	@ValidPhone
+	@Automapped
+	private String				phoneNumber;
+
+	@Mandatory
+	@ValidShortText
+	@Automapped
+	private String				specialization;
+
+	@Mandatory
+	@Automapped
+	private boolean				passedHealthTest;
+
+	@Mandatory
+	@ValidYearsOfExperience(min = 0, max = 70)
+	@Automapped
+	private Integer				yearsOfExperience;
+
+	@Optional
+	@ValidLongText
+	@Automapped
+	private String				certifications;
+}
