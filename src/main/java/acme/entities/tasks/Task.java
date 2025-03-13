ec5e82749ad4a9605baeb9a37c0ebc739fad98b6
Ivan Frutos <ivafrurai@alum.us.es>
2025-03-13 16:53:56 +0100

    132-Task-05: Implement Task Entity

---
+++ src/main/java/acme/entities/tasks/Task.java
@@ -0,0 +1,45 @@
+
+package acme.entities.tasks;
+
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidNumber;
+import acme.constraints.ValidLongText;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Task extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private TaskType			taskType;
+
+	@Mandatory
+	@ValidLongText
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@ValidNumber(min = 1, max = 10)
+	@Automapped
+	private Integer				priority;
+
+	@Mandatory
+	@ValidNumber(min = 1, max = 1000)
+	@Automapped
+	private Integer				estimatedDuration;
+}
