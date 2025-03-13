37863074092c06f2b5a1fa5d183fdf9e670faf0f
Juan <Juan@DESKTOP-KPGIL02>
2025-03-13 17:56:11 +0100

    Recommendations

---
+++ src/main/java/acme/entities/recommendations/Recommendations.java
@@ -0,0 +1,66 @@
+
+package acme.entities.recommendations;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.entities.airport.Airport;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Recommendations extends AbstractEntity {
+
+	private static final long		serialVersionUID	= 1L;
+
+	@Optional
+	@ValidString(min = 1, max = 50)
+	@Column(unique = false)
+	private String					amadeusId;
+
+	@Mandatory
+	@ValidString(min = 1, max = 255)
+	private String					name;
+
+	@Optional
+	@Enumerated(EnumType.STRING)
+	private RecommendationCategory	category;
+
+	@Optional
+	@ValidString(min = 0, max = 512)
+	private String					bookingLink;
+
+	@Optional
+	@ValidString(min = 0, max = 5000)
+	@Lob
+	private String					pictures;
+
+	@Optional
+	@ValidNumber(min = 0, max = 5)
+	private Double					rating;
+
+	@Optional
+	@ValidNumber(min = -90, max = 90)
+	private Double					latitude;
+
+	@Optional
+	@ValidNumber(min = -180, max = 180)
+	private Double					longitude;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airport					airport;
+}
